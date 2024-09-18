20deeac5435145ebbbf2046d95b0fd5d1c072e3d
Samit-51 <devilgamingbrawlstars@gmail.com>
2024-09-18 08:15:33 +0545

    Code updated

--- git.bash
+++ git.bash
@@ -4,6 +4,9 @@ git status && \
 
 echo -e '\n\nAdding files:'
 git add . && \
+
+echo -e '\n\nStatus:\n' && \
+git status && \
 echo -e '\n\nCommitting changes\n' && \
 git commit -m 'Code updated' && \
 
