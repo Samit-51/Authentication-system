475aaca6049ba1e645c80ee32e7ab8d776b1c3d1
Samit-51 <devilgamingbrawlstars@gmail.com>
2024-10-07 14:58:18 +0545

    Btg

--- frontend/src/Info/Hotel.jsx
+++ frontend/src/Info/Hotel.jsx
@@ -1,6 +1,9 @@
 const Hotel= ({Name, Id, Qr})=> {
   return(
       <div className= "hotelCard">
+        <button className="remove-btn">
+          <i className="fa-solid fa-trash"></i>
+        </button>
         <div className="img"></div>
         <div className= "info">
           <span className="Hotelname">{ Name }</span><br/>
